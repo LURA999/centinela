345e912ea14a72a91c31e460c352743b8992640e
alonso luna <alonsolr1999@gmail.com>
2022-08-03 18:09:33 -0700

    guardando cambios de solicitud ticket2

---
+++ src/app/models/contactsEmailTicket.model.ts
@@ -0,0 +1,9 @@
+export class contactsEmailTicket {
+    correo : string = ""
+    nombre : string = ""
+    correoCc : string[] = []
+    texto : string = ""
+    prioridad: string = ""
+    identificador : string = ""
+    servicio : string = ""
+}
