3486aaf1ddfd5685adbb5695da0be54648d08fbc
alonso luna <alonsolr1999@gmail.com>
2022-09-05 16:51:00 -0700

    guardando nuevo servico dashboardticket

---
+++ src/app/core/services/dashboardTickets.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { responseService } from 'src/app/models/responseService.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class dashboardTicketsService { 
+    api = environment.api;
+    constructor(private http : HttpClient) { }
+
+  rangoDeFechas(fechaInicio:string, fechaFinal : string, condicion:number):Observable<responseService>{
+    return this.http.get<responseService>(this.api+"Tickets/ticketDashboard.php?fechaInicio="+fechaInicio+"&fechaFin="+fechaFinal+"&tipo="+condicion)
+  }
+
+}
